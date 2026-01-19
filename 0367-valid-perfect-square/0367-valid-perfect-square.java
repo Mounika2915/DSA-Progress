c40fe405c46aebca3747cd69782851147e11a4a5
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-01-19 20:30:09 +0530

    Time: 0 ms (100%), Space: 42 MB (79.7%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1){
+            return true;
+        }
+        long l = 1 , r = num ;
+        while(l <= r){
+            long mid = l + (r-l)/2 ;
+            long val = mid * mid; 
+            if(val == num){
+                return true;
+            }else if (val > num){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+}
