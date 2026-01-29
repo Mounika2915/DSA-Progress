11f58ab7d58f428ffb5dfda155d536130ca420eb
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-01-29 06:20:56 +0530

    Time: 5 ms (65.92%), Space: 47.4 MB (12.41%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,30 +1,32 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        //3 cases
+        //positive positive nothing happens same direction.
+        //when negative comes absolute of smallest exploid.
+        //if both are same size both  exploid.
         int n = asteroids.length ;
         Stack<Integer> stk = new Stack<>();
         for(int i = 0 ; i < n ; i++){
-            if(asteroids[i] > 0 ){
-                stk.push(asteroids[i]) ;  
+            if(asteroids[i] > 0){
+                stk.push(asteroids[i]);
             }else{
-                while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < -asteroids[i]){
+                while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < Math.abs(asteroids[i])){
                     stk.pop();
                 }
                 if(stk.isEmpty() || stk.peek() < 0){
                     stk.push(asteroids[i]);
                 }
-
-                if(stk.peek() == -asteroids[i]){
+                if(stk.peek() == Math.abs(asteroids[i])){
                     stk.pop();
                 }
             }
-
-            
         }
         int[] res = new int[stk.size()];
-        int i = 0 ;
-        for(int val : stk){
-            res[i++] = val;
+        int x = 0 ;
+        for(int i : stk){
+            res[x] = i ; 
+            x++;
         }
-        return res ;
+        return res ; 
     }
 }
