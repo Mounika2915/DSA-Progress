9d9fb222f7e9854363f99c2ae52ad950dfa9db03
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-02-02 17:33:26 +0530

    Time: 88 ms (40.05%), Space: 95.5 MB (87.13%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,28 +1,19 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length ;
-        double[][] res = new double[n][2] ;
-        for(int i = 0 ; i < n ;i++){
-            res[i][0] = position[i] ;
-            res[i][1] =(double)(target - position[i])/speed[i] ;
+        double[][] res = new double[n][2];
+        for(int i =0 ; i< n ; i++){
+            res[i][0] = position[i];
+            res[i][1] = (double) ( target - position[i]) / speed[i];
         }
-        Arrays.sort(res , (a ,b)-> Double.compare(b[0] , a[0])) ;
-        Stack<Double> stk = new Stack<>();
-        stk.push(res[0][1]);
-        for(int i = 1 ; i < n ;i++){
-            if(stk.peek() < res[i][1]){
-                stk.push(res[i][1]);
+        Arrays.sort(res , (a,b) -> Double.compare(b[0] , a[0]));
+        double prevTime = 0 ;int  count = 0 ;
+        for(int i = 0 ;  i < n ;i++){
+            if(prevTime < res[i][1]){
+                prevTime = res[i][1];
+                count++;
             }
         }
-        return stk.size();
-        // double prevTime = 0;
-        // int count = 0 ;
-        // for(int i = 0 ; i < n ;i++){
-        //     if(prevTime < res[i][1]){
-        //         prevTime = res[i][1] ;
-        //         count++;
-        //     }
-        // }
-        // return count ;
+        return count ; 
     }
 }
