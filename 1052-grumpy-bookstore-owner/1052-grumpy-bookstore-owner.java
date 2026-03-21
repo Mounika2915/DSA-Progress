1e63c31727c10188eb1b47664b47fd080d8b3705
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-03-21 14:35:35 +0530

    Time: 3 ms (94.99%), Space: 48.7 MB (71.37%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int satisfied = 0 ;
-        for(int i = 0 ; i < grumpy.length ; i++){
-            if(grumpy[i] == 0){
-                satisfied += customers[i] ;
+        int satisfied = 0 ; 
+        for(int i = 0 ; i < customers.length ; i++){
+            if(grumpy[i] == 0 ){
+                satisfied += customers[i];
             }
         }
-        int unSatisfied = 0 ;
-        for(int i = 0 ; i < minutes ;i++){
+        int unSatisfied = 0 ; 
+        
+        for(int i = 0 ; i < minutes ; i++){
             if(grumpy[i] == 1){
-                unSatisfied += customers[i] ;
+                unSatisfied += customers[i] ; 
             }
         }
-        int l = 0 ; 
-        int max = unSatisfied;
-        for(int r = minutes ; r < grumpy.length ;r++){
-            if(grumpy[r] == 1){
-                unSatisfied += customers[r] ;
+        int l = 0 , max = unSatisfied ; 
+        for(int i = minutes ; i < customers.length ; i++){
+            if(grumpy[i] == 1){
+                unSatisfied += customers[i] ; 
             }
             if(grumpy[l] == 1){
-                unSatisfied -= customers[l] ;
+                unSatisfied -= customers[l] ; 
             }
             l++;
-            max = Math.max(max , unSatisfied);
+            max = Math.max(max , unSatisfied) ; 
         }
-        return max + satisfied ;
+        return max + satisfied ; 
     }
 }
