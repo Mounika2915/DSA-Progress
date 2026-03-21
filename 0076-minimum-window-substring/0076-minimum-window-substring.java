bfee40369072a31b69e29ecc6ff8fedee255fe1f
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-03-21 15:01:56 +0530

    Time: 3 ms (75.21%), Space: 45.3 MB (96.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] need = new int[128];
-    int[] window = new int[128];
-
-    for (char c : t.toCharArray()) {
-        need[c]++;
-    }
-
-    int needCount = 0;
-    for (int i = 0; i < 128; i++) {
-        if (need[i] > 0) needCount++;
-    }
-
-    int left = 0, right = 0, haveCount = 0;
-    int min = Integer.MAX_VALUE, startIdx = 0;
-
-    while (right < s.length()) {
-        char c = s.charAt(right);
-        window[c]++;
-
-        if (need[c] > 0 && window[c] == need[c]) {
-            haveCount++;
+        int[] need = new int[128] ; 
+        for(char c : t.toCharArray()){
+            need[c]++;
+        }
+        int needCount = 0 ; 
+        for(int i = 0 ; i < 128 ; i++){
+            if(need[i] > 0){
+                needCount++ ; 
+            }
         }
 
-        while (haveCount == needCount) {
-            if (right - left + 1 < min) {
-                min = right - left + 1;
-                startIdx = left;
+        int startIdx = 0 , haveCount = 0 , l = 0 , r = 0  , min = Integer.MAX_VALUE ; 
+        int[] window = new int[128] ; 
+        while(r < s.length()){
+            char c = s.charAt(r);
+            window[c]++ ; 
+            if(need[c] > 0 && need[c] == window[c]){
+                haveCount++;
             }
+            while(haveCount == needCount) {
+                if(r - l + 1 < min){
+                    min = r - l + 1 ; 
+                    startIdx = l ; 
+                }
+                char lc = s.charAt(l);
+                window[lc]-- ; 
+               
 
-            char leftChar = s.charAt(left);
-            window[leftChar]--;
-            if (need[leftChar] > 0 && window[leftChar] < need[leftChar]) {
-                haveCount--;
+                if(need[lc] > 0 && need[lc] > window[lc]){
+                    haveCount-- ; 
+                }
+                l++;
             }
-            left++;
+            r++;
         }
-
-        right++;
-    }
-
-    return min == Integer.MAX_VALUE ? "" : s.substring(startIdx, startIdx + min);
-
-
+        return min == Integer.MAX_VALUE ? "" : s.substring(startIdx , min + startIdx) ; 
     }
 }
