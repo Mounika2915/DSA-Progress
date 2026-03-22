13ea601c7e95f2f9f35f22c57e742cfaaa198b12
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-03-22 20:17:05 +0530

    Time: 2 ms (13.23%), Space: 43.6 MB (85.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,28 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim() ; 
-        if(s.isEmpty()){
-            return 0 ; 
+        int sign  = 1  , index = 0 ; long  val = 0 ; ; 
+        if(s.length() == 0){
+            return 0;
         }
-        int sign = 1 , index = 0 ;
-        long val = 0  ; 
-        if(s.charAt(0) == '-'){
+        if(s.charAt(index) == '-'){
             sign = -1 ; 
             index++;
-        }else if(s.charAt(0) == '+'){
-            index++; 
+        }else if(s.charAt(index) == '+'){
+            sign = 1 ; 
+            index++;  
         }
-
         while(index < s.length() && Character.isDigit(s.charAt(index))){
             int digit = s.charAt(index) - '0' ; 
             val = val * 10 + digit ; 
-            index++ ;
-            if(sign == 1 && val >= Integer.MAX_VALUE){
-                return Integer.MAX_VALUE ; 
-            }else if(sign == -1 && val * sign <= Integer.MIN_VALUE){
+            index++;
+            if(sign == -1 && val * sign <= Integer.MIN_VALUE){
                 return Integer.MIN_VALUE ; 
             }
+            if(sign == 1 && val >= Integer.MAX_VALUE){
+                return  Integer.MAX_VALUE ; 
+            }
         }
-        return (int) val  * sign ; 
+        return (int)val * sign ; 
     }
 }
