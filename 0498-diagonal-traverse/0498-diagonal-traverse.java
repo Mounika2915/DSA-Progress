232c0cf9dcc3e14e4358e86763abe09b7ae6bc01
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-04-06 22:57:36 +0530

    Time: 3 ms (74.61%), Space: 48.1 MB (55.73%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -2,38 +2,32 @@ class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int row = mat.length , col = mat[0].length ; 
         int[] res = new int[row * col];
-        int idx = 0 ; 
-        boolean direction = true ; 
-        for(int i = 0 ; i < col ;i++){
-            List<Integer> list = new ArrayList<>() ; 
-            int x = 0 , y = i ; 
-            while(x >= 0 && x < row && y >= 0 && y < col){
-                list.add(mat[x++][y--]);
-            }
-            if(direction){
-                Collections.reverse(list) ; 
-            }
-            direction = !direction ;
-            for(int l : list) {
-                res[idx++] = l;
-            }
-        }
-
-        for(int i = 1 ; i < row ;i++){
-             List<Integer> list = new ArrayList<>() ; 
-            int x = i , y = col - 1 ; 
-            while(x >= 0 && x < row && y >= 0 && y < col){
-                list.add(mat[x++][y--]);
-            }
-            if(direction){
-                Collections.reverse(list) ; 
-            }
-            direction = !direction ;
-            for(int l : list) {
-                res[idx++] = l;
+        
+        int r = 0 , c = 0 ; 
+        for(int i = 0 ; i < res.length ;i++){
+            res[i] = mat[r][c] ; 
+            if((r + c) % 2 == 0){
+                //moving up  means when sum is Even we have to move up
+                if(c == col -1){
+                    r++;
+                }else if(r == 0){
+                    c++;
+                }else{
+                    r--;
+                    c++ ; 
+                }
+            }else{
+                //moving down
+                if(r == row - 1 ){
+                    c++;
+                }else if(c == 0){
+                    r++;
+                }else{
+                    r++;
+                    c--;
+                }
             }
         }
-
-        return res ;
+        return res;
     }
 }
