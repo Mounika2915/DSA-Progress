16516a6d25f3ff9a88632d1cdc0d658c7b37071d
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-04-16 16:42:04 +0530

    Time: 1 ms (88.77%), Space: 43.5 MB (99.67%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,17 @@
 class Solution {
-    private void recurr(int[] nums , int i , int n , List<List<Integer>> list, List<Integer> curr){
-        if(i == n){
+    public void printSubSets(int[] nums , int idx , List<Integer> curr , List<List<Integer>> list){
+        if(idx >= nums.length){
             list.add(new ArrayList<>(curr)) ; 
-            return ; 
+            return  ;
         }
-        curr.add(nums[i]) ; 
-        recurr(nums, i+ 1 , n , list , curr) ; 
-        curr.remove(curr.size()-1) ; 
-        recurr(nums , i + 1 , n , list , curr) ; 
+        curr.add(nums[idx]) ; //take it
+        printSubSets(nums , idx + 1 , curr , list) ; 
+        curr.remove(curr.size() - 1) ;  // leave it
+        printSubSets(nums , idx + 1 , curr , list) ; 
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>() ; 
-        recurr(nums , 0 , nums.length  , list , new ArrayList<>()) ; 
+        printSubSets(nums ,  0 , new ArrayList<>() , list) ; 
         return list ; 
     }
 }
