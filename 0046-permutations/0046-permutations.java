b1c4435f6d5b9507ea49778b145722405ec2e325
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-04-20 16:19:15 +0530

    Time: 1 ms (85.92%), Space: 45.8 MB (19.67%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,46 @@
 class Solution {
-    public void f(int[] nums , List<Integer> curr  , List<List<Integer>> list , boolean[] freq ){
-        if(curr.size() == nums.length){
-            list.add(new ArrayList<>(curr)) ; 
+    // public void f(int[] nums , List<Integer> curr  , List<List<Integer>> list , boolean[] freq ){
+    //     if(curr.size() == nums.length){
+    //         list.add(new ArrayList<>(curr)) ; 
+    //         return ; 
+    //     }
+    //     for(int i = 0 ; i < nums.length ; i++){
+    //         if(!freq[i]){
+    //             curr.add(nums[i]) ; 
+    //             freq[i] = true ; 
+    //             f(nums, curr, list, freq) ; 
+    //             freq[i] = false ; 
+    //             curr.remove(curr.size() - 1) ; 
+    //         }
+    //     }
+    // }
+
+    private void f(int idx , int[] nums , List<List<Integer>> list){
+        if(idx == nums.length){
+            List<Integer> curr = new ArrayList<>() ; 
+            for(int i = 0 ; i < nums.length ; i++){
+                curr.add(nums[i]) ; 
+            }
+            list.add(new ArrayList<>(curr))  ; 
             return ; 
         }
-        for(int i = 0 ; i < nums.length ; i++){
-            if(!freq[i]){
-                curr.add(nums[i]) ; 
-                freq[i] = true ; 
-                f(nums, curr, list, freq) ; 
-                freq[i] = false ; 
-                curr.remove(curr.size() - 1) ; 
-            }
+        for(int i = idx ; i< nums.length ; i++){
+            swap(i , idx , nums) ; 
+            f(idx + 1 , nums , list) ; 
+            swap(i , idx , nums) ;
         }
+    }
+    private void swap(int i , int j , int[] nums){
+        int temp = nums[i] ; 
+        nums[i] = nums[j] ; 
+        nums[j]  = temp ; 
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length] ; 
-        f(nums , new ArrayList<>() , list , freq) ; 
+        // boolean[] freq = new boolean[nums.length] ; 
+        // f(nums , new ArrayList<>() , list , freq) ; 
+        // return list ; 
+        f(0 , nums , list) ; 
         return list ; 
     }
 }
