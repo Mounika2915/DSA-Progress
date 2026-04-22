1c1a05dcce31d7e77d3c2b2af3f5bc862497576a
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-04-22 18:07:30 +0530

    Time: 4 ms (62.68%), Space: 50.5 MB (13.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -26,7 +26,21 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
-        int min = f(m - 1 , n -1 , grid , dp) ;
-        return min ; 
+        //int min = f(m - 1 , n -1 , grid , dp) ;
+        //return min ; 
+
+        dp[0][0] = grid[0][0] ; 
+        for(int i = 1 ; i < m ; i++){
+            dp[i][0] = grid[i][0] + dp[i - 1][0] ; 
+        }
+        for(int j = 1 ; j < n ; j++){
+            dp[0][j] = grid[0][j] + dp[0][j-1] ; 
+        }
+        for(int i = 1 ; i< m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j] , dp[i][j-1]) ; 
+            }
+        }
+        return dp[m-1][n-1] ; 
     }
 }
