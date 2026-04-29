2a0a0236a00448d7e31db88497ad15a14df00813
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-04-29 08:27:54 +0530

    Time: 27 ms (25.73%), Space: 46.5 MB (41.32%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int helper(int[] coins , int amount , int[] dp){
+        if(amount == 0) {
+            return 0 ; 
+        }
+        if(amount < 0){
+            return Integer.MAX_VALUE ; 
+        }
+        if(dp[amount] != -1) return dp[amount] ; 
+        int min = Integer.MAX_VALUE ; 
+        for(int coin : coins){
+            int res = helper(coins , amount - coin , dp) ; 
+            
+            if(res != Integer.MAX_VALUE){
+                min = Math.min(min , res + 1) ; 
+            }
+        }
+        return dp[amount] = min ; 
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1] ; 
+        Arrays.fill(dp , -1) ; 
+        int res = helper(coins , amount ,dp ) ; 
+        return res == Integer.MAX_VALUE ? -1 : res ;
+     }
+} 
