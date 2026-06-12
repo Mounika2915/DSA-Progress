215489d7a1da5cfcb47f7b278e21ee16da7b6267
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-06-12 10:24:35 +0530

    Time: 3 ms (97.49%), Space: 48.6 MB (54.59%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        //1.first add the satisfied customeres
         int satisfied = 0 ; 
-        for(int i = 0 ; i < customers.length ; i++){
-            if(grumpy[i] == 0 ){
-                satisfied += customers[i];
+        for(int i = 0 ; i < grumpy.length ; i++){
+            if(grumpy[i] == 0){
+                satisfied += customers[i] ; 
             }
         }
         int unSatisfied = 0 ; 
-        
+        //now check for maximum consective customers for given minuets 
         for(int i = 0 ; i < minutes ; i++){
             if(grumpy[i] == 1){
                 unSatisfied += customers[i] ; 
             }
         }
-        int l = 0 , max = unSatisfied ; 
-        for(int i = minutes ; i < customers.length ; i++){
+        int max = unSatisfied ; 
+        for(int i = minutes ; i < grumpy.length ; i++){
             if(grumpy[i] == 1){
                 unSatisfied += customers[i] ; 
             }
-            if(grumpy[l] == 1){
-                unSatisfied -= customers[l] ; 
+            if(grumpy[i - minutes] == 1){
+                unSatisfied -= customers[i - minutes] ; 
             }
-            l++;
-            max = Math.max(max , unSatisfied) ; 
+            max = Math.max(max  , unSatisfied) ; 
         }
-        return max + satisfied ; 
+        return max + satisfied;
     }
 }
