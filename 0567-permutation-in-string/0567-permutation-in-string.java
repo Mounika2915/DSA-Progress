356e75318848665bd3c1d0e361e09d3991f0b70e
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-06-15 13:55:18 +0530

    Time: 6 ms (87.27%), Space: 44.1 MB (62.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] s1Arr = new int[26] ; 
-        for(char c : s1.toCharArray()){
-            s1Arr[c - 'a']++;
+        if(s1.length() > s2.length()){
+            return false; 
         }
-
-        int len = s1.length() ; 
-
-        int[] s2Arr = new int[26] ; 
-        for(int i = 0 ; i < s2.length() ; i++){
-            s2Arr[s2.charAt(i) - 'a']++ ; 
-            if(i >= len){
-                s2Arr[s2.charAt(i - len ) - 'a']-- ; 
-            }
+        int n = s1.length() ; 
+        int[] s1Arr = new int[128];
+        for(int i = 0 ; i < n ; i++){
+            s1Arr[s1.charAt(i)-'a']++;
+        }
+        int[] s2Arr = new int[128];
+        for(int i = 0 ; i < n ; i++){
+            s2Arr[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(s1Arr , s2Arr)){
+            return true ; 
+        }
+        for(int i = n ; i < s2.length() ; i++){
+            s2Arr[s2.charAt(i)-'a']++;
+            s2Arr[s2.charAt(i - n) - 'a']--;
             if(Arrays.equals(s1Arr , s2Arr)){
                 return true ; 
             }
         }
-        return false; 
+        return false ; 
     }
 }
