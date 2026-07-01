f78672349905ab405136afe6599b7f381d7ecb8d
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-07-01 15:26:18 +0530

    Time: 5 ms (68.6%), Space: 47.2 MB (30.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,32 +1,103 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        //3 cases
-        //positive positive nothing happens same direction.
-        //when negative comes absolute of smallest exploid.
-        //if both are same size both  exploid.
-        int n = asteroids.length ;
-        Stack<Integer> stk = new Stack<>();
-        for(int i = 0 ; i < n ; i++){
-            if(asteroids[i] > 0){
-                stk.push(asteroids[i]);
-            }else{
-                while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < Math.abs(asteroids[i])){
+
+        Stack<Integer> stk = new Stack<>() ; 
+        for(int i = 0 ; i < asteroids.length ; i++){
+            if(asteroids[i] >= 0 ){
+                stk.push(asteroids[i]) ; 
+            }else {
+                
+                while(!stk.isEmpty() && stk.peek() >= 0 && (stk.peek()) < Math.abs(asteroids[i])){
                     stk.pop();
                 }
-                if(stk.isEmpty() || stk.peek() < 0){
-                    stk.push(asteroids[i]);
+                if(stk.isEmpty() || stk.peek() < 0 ){
+                    stk.push(asteroids[i]) ; 
                 }
                 if(stk.peek() == Math.abs(asteroids[i])){
                     stk.pop();
                 }
             }
+            
+            
         }
-        int[] res = new int[stk.size()];
-        int x = 0 ;
-        for(int i : stk){
-            res[x] = i ; 
-            x++;
+        int[] res = new int[stk.size()] ; 
+        int i = 0 ; 
+        for(int s : stk){
+            res[i] = s ; 
+            i++;
         }
         return res ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //3 cases
+        //positive positive nothing happens same direction.
+        //when negative comes absolute of smallest exploid.
+        //if both are same size both  exploid.
+        // int n = asteroids.length ;
+        // Stack<Integer> stk = new Stack<>();
+        // for(int i = 0 ; i < n ; i++){
+        //     if(asteroids[i] > 0){
+        //         stk.push(asteroids[i]);
+        //     }else{
+        //         while(!stk.isEmpty() && stk.peek() > 0 && stk.peek() < Math.abs(asteroids[i])){
+        //             stk.pop();
+        //         }
+        //         if(stk.isEmpty() || stk.peek() < 0){
+        //             stk.push(asteroids[i]);
+        //         }
+        //         if(stk.peek() == Math.abs(asteroids[i])){
+        //             stk.pop();
+        //         }
+        //     }
+        // }
+        // int[] res = new int[stk.size()];
+        // int x = 0 ;
+        // for(int i : stk){
+        //     res[x] = i ; 
+        //     x++;
+        // }
+        // return res ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
     }
 }
