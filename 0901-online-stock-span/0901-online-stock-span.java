a910969ec35c20171d7a952c1e20ef65d6dcabaa
Mounika <155205651+Mounika2915@users.noreply.github.com>
2026-07-02 11:59:07 +0530

    Time: 31 ms (61.29%), Space: 54.5 MB (44.21%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,19 +1,17 @@
-
-
 class StockSpanner {
-   int i ;
-   Stack<int[]> stk ;
-    StockSpanner(){
-        i = 0 ; 
-        stk = new Stack<>();
+    Stack<int[]> stk ;
+    public StockSpanner() {
+        stk = new Stack<>() ;
     }
+    
     public int next(int price) {
-        int span = 1;
-        while(!stk.isEmpty() && price >= stk.peek()[0]){
-            span += stk.pop()[1];
+        int count = 1 ; 
+        while(!stk.isEmpty() && stk.peek()[0] <= price){
+            count += stk.peek()[1];
+            stk.pop();
         }
-        stk.push(new int[]{price , span});
-        return span ; 
+        stk.push(new int[] {price , count}) ; 
+        return count;
     }
 }
 
